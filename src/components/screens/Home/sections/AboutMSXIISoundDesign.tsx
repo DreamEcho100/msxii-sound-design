3a48bbcc63e3c9a751cdd4af0ad1b9bb37b3d736
DreamEcho100 <maze6572198@gmail.com>
2023-02-22 19:53:24 +0200

    Minor update

--- src/components/screens/Home/sections/AboutMSXIISoundDesign.tsx
+++ src/components/screens/Home/sections/AboutMSXIISoundDesign.tsx
@@ -9,7 +9,7 @@ const AboutMSXIISoundDesign = (props: Props) => {
       <div className="flex flex-col-reverse justify-center gap-4 bg-special-secondary-500 px-main-p-2 sm:gap-12 sm:rounded-xl lg:flex-row lg:px-main-p-1">
         <div
           className="lg:text-align-initial flex flex-col 
-gap-8 pb-main-p-2 text-center text-lg lg:w-1/2 lg:max-w-screen-sm lg:pt-main-p-1"
+gap-8 pb-main-p-2 text-center text-lg font-medium lg:w-1/2 lg:max-w-screen-sm lg:pt-main-p-1"
         >
           <header>
             <h2 className="text-h1 font-black">About MSXII Sound Design</h2>

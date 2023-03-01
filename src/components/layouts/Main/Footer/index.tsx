8285fdac74dfc553db1d9082b232be5729f1effa
DreamEcho100 <maze6572198@gmail.com>
2023-03-01 14:04:15 +0200

    Adding some content to the footer

--- src/components/layouts/Main/Footer/index.tsx
+++ src/components/layouts/Main/Footer/index.tsx
@@ -5,6 +5,44 @@ import Clickable from '~/components/shared/core/Clickable';
 import { motion } from 'framer-motion';
 import { cx } from 'class-variance-authority';
 import { useGlobalStore } from '~/utils/store';
+import { MdEmail } from 'react-icons/md';
+import Link from 'next/link';
+
+const footerLinks = [
+	{
+		text: 'Collections',
+		links: [
+			{ text: 'New Releases', href: '/', icon: null },
+			{ text: 'Loops', href: '/', icon: null },
+			{ text: 'One shot drums', href: '/', icon: null },
+			{ text: 'Sample Packs', href: '/', icon: null },
+			{ text: 'Drum Kits', href: '/', icon: null },
+			{ text: 'Construction Kits', href: '/', icon: null },
+			{ text: 'Presets', href: '/', icon: null },
+			{ text: 'Bundles', href: '/', icon: null }
+		]
+	},
+	{
+		text: 'Navigation',
+		links: [
+			{ text: 'Search', href: '/', icon: null },
+			{ text: 'Creative Space', href: '/', icon: null },
+			{ text: 'About Us', href: '/', icon: null },
+			{ text: 'Support', href: '/', icon: null },
+			{ text: 'License Agreement', href: '/', icon: null }
+		]
+	},
+	{
+		text: 'Contact Us',
+		links: [
+			{
+				text: 'support@msxaudio.com',
+				href: 'mailto:support@msxaudio.com',
+				icon: <MdEmail />
+			}
+		]
+	}
+];
 
 const MainLayoutFooter = () => {
 	const mainFooterRef = useRef<HTMLElement>(null);
@@ -31,70 +69,139 @@ const MainLayoutFooter = () => {
 
 	return (
 		<footer id="main-footer" ref={mainFooterRef}>
-			<div className="mx-auto flex max-w-main flex-grow flex-wrap items-center justify-around gap-x-8 gap-y-4 py-main-p-4 px-main-p-3 sm:px-main-p-2 text-center font-normal sm:justify-between">
-				<div className="">
-					<small>&copy; 2023 MSXII Sound</small>
+			<div className="mx-auto flex flex-col gap-4 max-w-main py-main-p-4 px-main-p-3 sm:px-main-p-2 text-text-primary-400 dark:text-text-primary-600">
+				<div className="flex flex-wrap justify-between gap-4">
+					{footerLinks.map((item) => (
+						<ul key={item.text} className="flex flex-col gap-2">
+							<li>
+								<h3 className="text-h6 dark:text-text-primary-500">
+									{item.text}
+								</h3>
+							</li>
+							{item.links.map((link) => (
+								<li key={link.text}>
+									<Link
+										href={link.href}
+										className={cx(
+											'flex flex-wrap sm:flex-nowrap items-center gap-1 w-fit border-b-[0.125rem] border-solid border-b-transparent outline-none',
+											'duration-150 transition-all',
+											'hover:text-text-primary-500',
+											'focus:border-b-text-primary-200 focus:text-text-primary-500',
+											'focus:hover:border-b-text-primary-300'
+										)}
+									>
+										{link.icon}
+										{link.text}
+									</Link>
+								</li>
+							))}
+						</ul>
+					))}
+					<div className="flex flex-col gap-4 flex-grow max-w-sm">
+						<header className="flex flex-col gap-2">
+							<h3 className="text-h6 dark:text-text-primary-500">
+								Join our newsletter
+							</h3>
+							<p>New Subscribers get our Site Sampler Free!</p>
+						</header>
+						<form className="flex flex-col gap-6">
+							<input
+								type="text"
+								placeholder="Enter your email address"
+								className="bg-transparent border-b-[0.125rem] border-solid border-b-text-primary-200 py-2 outline-none"
+							/>
+							<Clickable
+								variants={{ rounded: 'md' }}
+								className="text-h5 uppercase font-medium"
+							>
+								subscribe
+							</Clickable>
+						</form>
+					</div>
 				</div>
-				<div className="">
-					<Clickable href="/" isA="next-js">
-						<Image src="/images/logo.png" alt="logo" width="60" height="48" />
-					</Clickable>
-				</div>
-				<div className="flex flex-wrap items-center justify-center gap-2">
-					<Clickable
-						variants={null}
-						className={cx(
-							'w-12 h-6 flex justify-start items-center px-1 cursor-pointer rounded-[2.5rem] bg-text-primary-300/40',
-							isDarkTheme ? 'justify-end bg-text-primary-500/40' : undefined
-						)}
-						onClick={() => changeCurrentTheme()}
-						title={`Set theme to ${isDarkTheme ? 'light' : 'dark'} mode.`}
-					>
-						<motion.div
+				<div className="mx-auto flex max-w-main w-full flex-grow flex-wrap items-center justify-around gap-x-8 gap-y-4 text-center font-normal sm:justify-between">
+					<div className="">
+						<small>
+							&copy;&nbsp;2023&nbsp;
+							<Link
+								href="/"
+								className="border-b-[0.125rem] border-solid border-b-text-primary-200"
+							>
+								MSXII Sound
+							</Link>
+						</small>
+					</div>
+					<div className="">
+						<Clickable href="/" isA="next-js">
+							<Image src="/images/logo.png" alt="logo" width="60" height="48" />
+						</Clickable>
+					</div>
+					<div className="flex flex-wrap items-center justify-center gap-2">
+						<Clickable
+							variants={null}
 							className={cx(
-								'w-4 h-4 bg-text-primary-0 rounded-[50%]',
-								isDarkTheme ? 'bg-text-primary-1000/50' : ''
+								'w-12 h-6 flex justify-start items-center px-1 cursor-pointer rounded-[2.5rem] bg-text-primary-300/40',
+								isDarkTheme ? 'justify-end bg-text-primary-500/40' : undefined
 							)}
-							layout
-							transition={{
-								type: 'spring',
-								stiffness: 700,
-								damping: 30
-							}}
-						/>
-					</Clickable>
-					<ul className="flex flex-wrap gap-2">
-						<li>
-							<Clickable
-								isA="basic-link"
-								href="/"
-								className=""
-								title="facebook"
-							>
-								<FaFacebookF />
-							</Clickable>
-						</li>
-						<li>
-							<Clickable isA="basic-link" href="/" className="" title="twitter">
-								<FaTwitter />
-							</Clickable>
-						</li>
-						<li>
-							<Clickable isA="basic-link" href="/" className="" title="youtube">
-								<FaYoutube />
-							</Clickable>
-						</li>
-						<li>
-							<Clickable
-								isA="basic-link"
-								href="/"
-								className=""
-								title="instagram"
-							>
-								<FaInstagram />
-							</Clickable>
-						</li>
-					</ul>
+							onClick={() => changeCurrentTheme()}
+							title={`Set theme to ${isDarkTheme ? 'light' : 'dark'} mode.`}
+						>
+							<motion.div
+								className={cx(
+									'w-4 h-4 bg-text-primary-0 rounded-[50%]',
+									isDarkTheme ? 'bg-text-primary-1000/50' : ''
+								)}
+								layout
+								transition={{
+									type: 'spring',
+									stiffness: 700,
+									damping: 30
+								}}
+							/>
+						</Clickable>
+						<ul className="flex flex-wrap gap-2">
+							<li>
+								<Clickable
+									isA="basic-link"
+									href="/"
+									className=""
+									title="facebook"
+								>
+									<FaFacebookF />
+								</Clickable>
+							</li>
+							<li>
+								<Clickable
+									isA="basic-link"
+									href="/"
+									className=""
+									title="twitter"
+								>
+									<FaTwitter />
+								</Clickable>
+							</li>
+							<li>
+								<Clickable
+									isA="basic-link"
+									href="/"
+									className=""
+									title="youtube"
+								>
+									<FaYoutube />
+								</Clickable>
+							</li>
+							<li>
+								<Clickable
+									isA="basic-link"
+									href="/"
+									className=""
+									title="instagram"
+								>
+									<FaInstagram />
+								</Clickable>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</footer>

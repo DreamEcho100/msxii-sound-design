9f955a42c651f8b1df94000c100932971c91ad91
DreamEcho100 <maze6572198@gmail.com>
2023-04-13 15:39:27 +0200

    Minor update

--- src/pages/merch/temp-preview.tsx
+++ src/pages/merch/temp-preview.tsx
@@ -19,7 +19,19 @@ const TempPreviewProductPage = () => {
 			productData={productData}
 			medias={medias}
 			products={products}
-		/>
+		>
+			<section className="w-full mx-auto max-w-[140ch] flex flex-col py-16 gap-16">
+				<div className="px-8 mx-auto max-w-[131ch] flex flex-col gap-4">
+					<h2 className="font-normal text-text-primary-400 text-h3">Details</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Necessitatibus amet tempore delectus voluptatibus perspiciatis, et
+						tempora non, deserunt molestias sint unde at debitis obcaecati nobis
+						incidunt asperiores. Fugit, doloremque voluptates.
+					</p>
+				</div>
+			</section>
+		</CustomProductScreen>
 	);
 };
 

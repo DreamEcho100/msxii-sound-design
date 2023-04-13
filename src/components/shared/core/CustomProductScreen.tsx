8c95c7323d66066d70f1be8f8bdf4f6980cfe422
DreamEcho100 <maze6572198@gmail.com>
2023-04-13 15:37:58 +0200

    Minor update

--- src/components/shared/core/CustomProductScreen.tsx
+++ src/components/shared/core/CustomProductScreen.tsx
@@ -1,13 +1,10 @@
 import { cx } from 'class-variance-authority';
 
-import { HTMLAttributes, useState } from 'react';
-
-import { BiPlay } from 'react-icons/bi';
+import { HTMLAttributes, ReactNode, useState } from 'react';
 
 import { SwiperSlide } from 'swiper/react';
 
 import CustomNextImage from '../CustomNextImage';
-import { YouTubeIFrame } from '../Iframes';
 
 import Clickable from './Clickable';
 import ProductPrice from './ProductPrice';
@@ -194,9 +191,10 @@ const ProductImageShowcase = ({
 
 const CustomProductScreen = ({
 	productData,
-	medias,
+	children,
 	products
 }: {
+	children?: ReactNode;
 	productData: ShopifyProduct;
 	medias: {
 		alt: null;
@@ -264,74 +262,7 @@ const CustomProductScreen = ({
 				</div>
 				<ProductImageShowcase productData={productData} />
 			</section>
-			<section className="w-full mx-auto max-w-[140ch] flex flex-col py-16 gap-16">
-				<div className="px-8 mx-auto max-w-[131ch] flex flex-col gap-4">
-					<h2 className="font-normal text-text-primary-400 text-h3">Details</h2>
-					<p>
-						35 original loops ready to rock in various tempos, keys, and with
-						numerous textures and colors. Processed tastefully to add warmth,
-						depth, and a creative touch, Loops Go Crazy Vol. 5 boasts more
-						creativity, better textures and the MSXII authentication you&apos;re
-						looking for!
-					</p>
-					<p>
-						When you need the best, tap in with MSXII. Purchase now & be a
-						champ. Demo showcases a selection of these samples in the pack!
-					</p>
-				</div>
-				<YouTubeIFrame
-					containerProps={{
-						className: 'w-full rounded-3xl overflow-hidden relative isolate'
-					}}
-					overlayImageProps={{
-						src: productData.featured_image,
-						alt: productData.title
-					}}
-					width="550"
-					height="550"
-					src="https://www.youtube.com/embed/TDI_97_Gu6c"
-					title="YouTube video player"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				/>
-			</section>
-			<section className="w-full mx-auto max-w-[140ch] bg-initial-primary-0 text-initial-primary-500 flex flex-col gap-10 rounded-3xl py-12 px-16">
-				<h2 className="font-normal text-initial-primary-500 text-h3">
-					Preview Samples
-				</h2>
-				<div className="flex flex-col gap-8">
-					{medias.map((media) => (
-						<article key={productData.id} className="flex gap-8">
-							<div className="w-24 h-24 aspect-square overflow-hidden rounded-sm relative">
-								<CustomNextImage
-									src={media.src}
-									alt={media.alt || ''}
-									width={800}
-									height={800}
-									className="w-full h-full object-cover"
-								/>
-								<Clickable
-									className="hover:-translate-y-[60%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
-									variants={{
-										px: 'sm',
-										py: 'sm',
-										rounded: 'full',
-										transform: null
-									}}
-								>
-									<BiPlay className="text-2xl translate-x-[7.5%] scale-110 rtl:-translate-x-[5%] rtl:rotate-180" />
-								</Clickable>
-							</div>
-							<div className="flex flex-col gap-2 flex-grow py-2">
-								<div className="rounded-sm py-2 bg-special-primary-500" />
-								<h3 className="font-normal text-initial-primary-400 text-[100%]">
-									Lorem ipsum
-								</h3>
-							</div>
-						</article>
-					))}
-				</div>
-				<p>+ so many more high quality samples</p>
-			</section>
+			{children}
 			<article>
 				<CTAButton
 					text="Explore more high quality packs"

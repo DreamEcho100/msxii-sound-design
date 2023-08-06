6308574de8468137e067fe3f503b1ccaac69b6ac
DreamEcho100 <maze6572198@gmail.com>
2023-08-07 00:45:19 +0300

    feat: adding a constraint for the product price/comparePrice

--- src/components/shared/core/Shopify/ProductPrice/index.tsx
+++ src/components/shared/core/Shopify/ProductPrice/index.tsx
@@ -1,38 +1,45 @@
-import { useMemo } from 'react';
-import { type MoneyV2 } from 'shopify-buy';
-import { useIsMounted } from '~/utils/hooks';
-import { formatPrice } from '~/utils/shopify';
+import { useMemo } from "react";
+import { type MoneyV2 } from "shopify-buy";
+import { useIsMounted } from "~/utils/hooks";
+import { formatPrice } from "~/utils/shopify";
 
 type Props = {
-	price: MoneyV2;
-	compareAtPrice?: MoneyV2;
+  price: MoneyV2;
+  compareAtPrice?: MoneyV2;
 };
 
 const ProductPrice = (props: Props) => {
-	const isMounted = useIsMounted();
-	const { price, compareAtPrice } = useMemo(() => {
-		const price = formatPrice(
-			Number(props.price.amount),
-			props.price.currencyCode,
-			isMounted,
-		);
-		const compareAtPrice = props.compareAtPrice?.amount
-			? formatPrice(
-					Number(props.compareAtPrice.amount),
-					props.compareAtPrice.currencyCode,
-					isMounted,
-			  )
-			: null;
+  const isMounted = useIsMounted();
+  const { price, compareAtPrice } = useMemo(() => {
+    const price = formatPrice(
+      Number(props.price.amount),
+      props.price.currencyCode,
+      isMounted
+    );
+    const compareAtPrice = props.compareAtPrice?.amount
+      ? formatPrice(
+          Number(props.compareAtPrice.amount),
+          props.compareAtPrice.currencyCode,
+          isMounted
+        )
+      : null;
 
-		return { price, compareAtPrice };
-	}, [props.price, props.compareAtPrice, isMounted]);
+    return {
+      price,
+      compareAtPrice: compareAtPrice
+        ? compareAtPrice === price || parseFloat(compareAtPrice) < Number(price)
+          ? undefined
+          : compareAtPrice
+        : undefined,
+    };
+  }, [props.price, props.compareAtPrice, isMounted]);
 
-	return (
-		<>
-			{price}{' '}
-			{compareAtPrice && <del className="text-red-500">{compareAtPrice}</del>}
-		</>
-	);
+  return (
+    <>
+      {price}{" "}
+      {compareAtPrice && <del className="text-red-500">{compareAtPrice}</del>}
+    </>
+  );
 };
 
 export default ProductPrice;

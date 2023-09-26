877cec47e82b02ab9a069611706af5dbd79930b4
DreamEcho100 <maze6572198@gmail.com>
2023-09-26 05:06:12 +0300

    feat: updating headers

--- next.config.mjs
+++ next.config.mjs
@@ -52,10 +52,10 @@ const config = {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self'",
-          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: "frame-ancestors 'self'",
+          // },
           // {
           //   key: "X-XSS-Protection",
           //   value: "1; mode=block",

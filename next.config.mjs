999748313f46d112dd79ddf7daa9a201088750d4
DreamEcho100 <maze6572198@gmail.com>
2023-09-26 05:00:32 +0300

    feat: updating headers

--- next.config.mjs
+++ next.config.mjs
@@ -59,8 +59,13 @@ const config = {
             key: "X-XSS-Protection",
             value: "1; mode=block",
           },
+        ],
+      },
+      {
+        source: "/api/(.*)",
+        headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",

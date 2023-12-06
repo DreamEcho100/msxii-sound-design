2267ea23c2a8bb8d19a08bfc49edef1da670ba48
DreamEcho100 <maze6572198@gmail.com>
2023-12-06 19:59:02 +0200

    feat: minor update

--- src/server/libs/drizzle/db/queryClient.ts
+++ src/server/libs/drizzle/db/queryClient.ts
@@ -1,13 +1,23 @@
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import drizzleSchemaWithRelations from "./SchemaWithRelations";
 import { z } from "zod";
 
-const pool = postgres(z.string().min(1).parse(process.env.DATABASE_URL));
+const globalForPrisma = globalThis as unknown as {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  drizzleQueryClient: PostgresJsDatabase<typeof drizzleSchemaWithRelations>;
+};
 
-const drizzleQueryClient = drizzle(pool, {
-  schema: drizzleSchemaWithRelations,
-  logger: !!process.env.IsDrizzleLoggerOn,
-});
+const drizzleQueryClient = (() => {
+  if (globalForPrisma.drizzleQueryClient)
+    return globalForPrisma.drizzleQueryClient;
+
+  const pool = postgres(z.string().min(1).parse(process.env.DATABASE_URL));
+  drizzle(pool, {
+    schema: drizzleSchemaWithRelations,
+    logger: !!process.env.IsDrizzleLoggerOn,
+  });
+})();
 
 export default drizzleQueryClient;
